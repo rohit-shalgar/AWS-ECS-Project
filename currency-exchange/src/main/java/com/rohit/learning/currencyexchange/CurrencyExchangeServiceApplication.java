89cbbf49a43890a5c175a470632d7c44d582c7f0
rohit-shalgar <learn.rohits@.github.com>
2022-06-01 21:20:27 -0500

    Xray -added more dependencies

--- currency-exchange/src/main/java/com/rohit/learning/currencyexchange/CurrencyExchangeServiceApplication.java
+++ currency-exchange/src/main/java/com/rohit/learning/currencyexchange/CurrencyExchangeServiceApplication.java
@@ -1,14 +1,33 @@
 package com.rohit.learning.currencyexchange;
 
+import com.rohit.learning.currencyexchange.entity.CurrencyExchange;
+import com.rohit.learning.currencyexchange.repository.CurrencyExchangeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
-public class CurrencyExchangeServiceApplication {
+public class CurrencyExchangeServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	private CurrencyExchangeRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		if(repository.count() == 0){
+			repository.save(new CurrencyExchange(10001, "USD", "INR", BigDecimal.valueOf(60),""));
+			repository.save(new CurrencyExchange(10002, "EUR", "INR", BigDecimal.valueOf(70),""));
+			repository.save(new CurrencyExchange(10003, "AUD", "INR", BigDecimal.valueOf(20),""));
+		}
+
+	}
 }
--- currency-exchange/src/main/java/com/rohit/learning/currencyexchange/restcontroller/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/rohit/learning/currencyexchange/restcontroller/CurrencyExchangeController.java
@@ -1,37 +1,54 @@
 package com.rohit.learning.currencyexchange.restcontroller;
 
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 import com.rohit.learning.currencyexchange.entity.CurrencyExchange;
 import com.rohit.learning.currencyexchange.exception.CurrencyExchangeExceptionMessages;
 import com.rohit.learning.currencyexchange.exception.CurrencyExchangePairNotFoundException;
+import com.rohit.learning.currencyexchange.service.ContainerMetadataService;
 import com.rohit.learning.currencyexchange.service.CurrencyExchangeService;
 import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
+@XRayEnabled
 @SuppressWarnings("unused")
 public class CurrencyExchangeController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
-    private Environment environment;
+    private ContainerMetadataService metadataService;
 
     @Autowired
     private CurrencyExchangeService currencyExchangeService;
 
     @GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange getConversionMultiple(@PathVariable String from, @PathVariable String to) {
-        String port = environment.getProperty("local.server.port");
-        val currencyExchange = currencyExchangeService.getCurrencyExchange(from,to);
-        if(Objects.nonNull(currencyExchange)){
-            currencyExchange.setEnvironment(port);
+    public CurrencyExchange getConversionMultiple(@PathVariable String from,
+                                                  @PathVariable String to,
+                                                  @RequestHeader Map<String,String> headers) {
+        printAllHeaders(headers);
+        val currencyExchange = currencyExchangeService.getCurrencyExchange(from, to);
+        if (Objects.nonNull(currencyExchange)) {
+            currencyExchange.setEnvironment(metadataService.retrieveContainerMetadataInfo());
             return currencyExchange;
         }
-            throw new CurrencyExchangePairNotFoundException(CurrencyExchangeExceptionMessages.CURRENCY_NOT_FOUND+from+":"+to);
+        throw new CurrencyExchangePairNotFoundException(CurrencyExchangeExceptionMessages.CURRENCY_NOT_FOUND + from + ":" + to);
+    }
+
+    private void printAllHeaders(Map<String, String> headers) {
+        for (Map.Entry<String,String> headerEntry:headers.entrySet()){
+            LOGGER.info(String.format("Header - '%s' - '%s'",headerEntry.getKey(),headerEntry.getValue()));
+        }
     }
 
 }
